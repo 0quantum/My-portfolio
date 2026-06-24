1dee00efd6044747ce1eb30d1505d54f194045f6
Nick <114471925+iamjustaprogrammer@users.noreply.github.com>
2026-06-24 03:17:52 +0300

    fixed deployment bugs

--- data/index.ts
+++ data/index.ts
@@ -80,11 +80,11 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
+    title: "GERICHT - Restaurant-React-App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://0quantum.github.io/Restaurant-React-App/",
   },
   {
     id: 3,
